52c09da05ef3a589fee1c960e3553e6a2ea508e3
Anthony Rodriguez <anthonyrodriguez0506@outlook.com>
2025-02-17 09:28:58 -0400

    Add title exercise

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,3 +1,8 @@
+// Print the numbers from 1 to 100.
+// For multiples of 3, print "Fizz".
+// For multiples of 5, print "Buzz".
+// For multiples of both 3 and 5, print "FizzBuzz".
+
 for (let i = 1; i <= 100; i++) {
     if (i % 3 === 0 && i % 5 === 0) {
         console.log(`${i} FizzBuzz`);
