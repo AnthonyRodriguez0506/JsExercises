a36fe7cdebe4718ded3a52961646188469e9c7c3
Anthony Rodriguez <anthonyrodriguez0506@outlook.com>
2025-02-17 10:19:53 -0400

    add new file

---
+++ RandomNumber.js
@@ -0,0 +1,9 @@
+function getRandomNumber() {
+    const random = Math.random()
+    const multiplied = random * 10
+    const rounded = Math.floor(multiplied)
+    const result = rounded + 1
+    return result
+  }
+
+getRandomNumber()
