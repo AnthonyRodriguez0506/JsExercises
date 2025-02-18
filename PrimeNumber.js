a2187503f3472438a7665e28350a20c571c68b05
Anthony Rodriguez <anthonyrodriguez0506@outlook.com>
2025-02-18 10:19:12 -0400

    add new file

---
+++ PrimeNumber.js
@@ -0,0 +1,18 @@
+function PrimeNumber(N) {
+    if (N < 2) {
+        console.log(`${N} no es primo`);
+        return;
+    }
+
+    for (let i = 2; i <= Math.sqrt(N); i++) {
+        if (N % i === 0) {
+            console.log(`${N} no es primo`);
+            return;
+        }
+    }
+
+    console.log(`${N} es primo`);
+}
+
+PrimeNumber(1)
+PrimeNumber(2)
