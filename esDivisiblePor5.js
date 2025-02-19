880a7efe5cd277e584b6d36c4260158da3957591
Anthony Rodriguez <anthonyrodriguez0506@outlook.com>
2025-02-19 09:17:10 -0400

    New File

---
+++ esDivisiblePor5.js
@@ -0,0 +1,10 @@
+function esDivisiblePor5 (n) {
+
+    if (n % 5 === 0) {
+        console.log(`${n} es divisible entre 5`);
+    } else {
+        console.log(`${n} no es divisible entre 5`);
+    }
+ }
+ 
+ esDivisiblePor5 (100);
